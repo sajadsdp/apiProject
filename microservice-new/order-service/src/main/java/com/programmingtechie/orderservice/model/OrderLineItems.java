d8f5f6237af9dfefc730bd9e6ac636ab08abcbf2
sajad <sajad.sdp3@gmail.com>
2023-09-28 16:52:28 +0330

    first build

--- microservice-new/order-service/src/main/java/com/programmingtechie/orderservice/model/OrderLineItems.java
+++ microservice-new/order-service/src/main/java/com/programmingtechie/orderservice/model/OrderLineItems.java
@@ -1,12 +1,13 @@
 package com.programmingtechie.orderservice.model;
 
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
+
 
 @Entity
 @Table(name = "t_order_line_items")
