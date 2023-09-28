478e71f8875b53d9308f6e502e11b11062d36244
sajad <sajad.sdp3@gmail.com>
2023-09-28 19:20:56 +0330

    discovery server

--- microservice-new/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
+++ microservice-new/order-service/src/main/java/com/programmingtechie/orderservice/service/OrderService.java
@@ -22,7 +22,7 @@ import java.util.UUID;
 public class OrderService {
 
     private final OrderRepository orderRepository;
-    private final WebClient webClient;
+    private final WebClient.Builder webClientBuilder;
 
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
@@ -35,8 +35,8 @@ public class OrderService {
 
         List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
 
-        InventoryResponse[] inventoryResponsesArray = webClient.get()
-                .uri("http://localhost:8082/api/inventory",
+        InventoryResponse[] inventoryResponsesArray = webClientBuilder.build().get()
+                .uri("http://inventory-service/api/inventory",
                          uriBuilder -> uriBuilder.queryParam("skuCode" , skuCodes).build())
                 .retrieve()
                 .bodyToMono(InventoryResponse[].class)
