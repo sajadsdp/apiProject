1a64dc9371db2cd9aeabd82c4a5f324371c90709
sajad <sajad.sdp3@gmail.com>
2023-09-28 16:32:03 +0330

    base on microservice project

--- microservices-parent/product-service/src/main/java/com/programmeringtechie/productservice/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.programmeringtechie.productservice.repository;
-
-import com.programmeringtechie.productservice.model.Product;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface ProductRepository extends MongoRepository<Product,String>{
-}
--- mongoTest/src/main/java/com/example/mongotest/redis1/UserRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.mongotest.redis1;
-
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRedisRepository extends CrudRepository<UserRedis, Integer> {
-}
